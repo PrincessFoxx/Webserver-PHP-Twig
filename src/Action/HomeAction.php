e16b2b7493a53fe631984cebf6bf4b536129eaf9
PrincessFoxx <63935304+PrincessFoxx@users.noreply.github.com>
2023-02-25 20:30:13 -0600

    final

--- src/Action/HomeAction.php
+++ src/Action/HomeAction.php
@@ -5,13 +5,29 @@ namespace Project\Name\Action;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Views\Twig;
+use Project\Name\Core\Math;
 
 final class HomeAction {
   public function __construct(
-    private Twig $view
+    private Twig $view,
   ){}
 
   public function __invoke(Request $request, Response $response): Response {
-    return $this->view->render($response, 'home.twig');
+    $a = 20;
+    $b = 2;
+    $sum = Math::add($a, $b);
+    $formula1 = "$a + $b = $sum";
+
+    $product = Math::multiply($a, $b);
+    $formula2 = "$a * $b = $product";
+
+    $factoral = Math::factorial($b);
+    $formula3 = "$b! = $factoral";
+    
+    return $this->view->render($response, 'home.twig', [
+      'add' => $formula1,
+      'multiply' => $formula2,
+      'factorial' => $formula3,
+    ]);
   }
 }
